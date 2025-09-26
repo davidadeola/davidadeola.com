9329587a655ab8ccacedc136be6b6d1c7e8a4dc4
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-09-26 05:09:45 +0100

    feat: add global error boundary to catch errors globally and present a more friendly interface

---
+++ src/app/global-error.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Button } from "@/components/button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <html>
+      <body>
+        <div className="flex flex-col border min-h-screen items-center justify-center">
+          <h2 className="text-xl font-bold mb-4">Something went wrong!</h2>
+          <p className="text-gray-600 mb-6">
+            We couldn’t complete your request. You can try again or return home.
+          </p>
+
+          <div className="flex gap-4">
+            <Button onClick={() => reset()} className="px-4 !py-2">
+              Try Again
+            </Button>
+
+            <Link href="/" className="px-4 py-1 border">
+              Go Home
+            </Link>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
